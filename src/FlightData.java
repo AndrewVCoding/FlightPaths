8d5c994c5be640777263e44cb126ee15738d1c77
AndrewVCoding <andrew@txvaughn.com>
2017-07-25 14:14:00 -0500

    The program now correctly sorts the paths and only prints out the three cheapest paths for each trip, whether by cost or time. Now need to implement reading the data from txt files.

--- src/FlightData.java
+++ src/FlightData.java
@@ -37,7 +37,7 @@ class FlightData
 	/**
 	 * Returns the cost of going from the source to the destination City
 	 *
-	 * @param source The starting city
+	 * @param source      The starting city
 	 * @param destination The ending city
 	 * @return The cost from the source to an adjacent destination
 	 */
@@ -65,7 +65,7 @@ class FlightData
 	/**
 	 * Returns the time to go from the source to the destination City
 	 *
-	 * @param source The starting city
+	 * @param source      The starting city
 	 * @param destination The ending city
 	 * @return The time from the source to an adjacent destination
 	 */
@@ -93,11 +93,12 @@ class FlightData
 	/**
 	 * Returns a list of all possible paths from startIn to destinationIn
 	 *
-	 * @param startIn The name of the starting city
+	 * @param startIn       The name of the starting city
 	 * @param destinationIn The name of the destination city
+	 * @param time          True sorts the paths by time, False sorts the paths by cost
 	 * @return A list of strings that each represent a path from the starting to the ending city
 	 */
-	public List<String> printFlightPaths(String startIn, String destinationIn)
+	public List<String> printFlightPaths(String startIn, String destinationIn, boolean time)
 	{
 		boolean debug = false;
 		List<String> output = new ArrayList<>();
@@ -118,27 +119,32 @@ class FlightData
 		//Get a list of paths from the source to the destination
 		getPaths(path, dest, paths);
 
+		paths = sortPaths(paths, time);
+
 		//Build a string displaying the flight path and add it to the list of paths
 		int pathNum = 0;
 		for(List<City> flightPlan : paths)
 		{
-			COST = getPathCost(flightPlan);
-			TIME = getPathTime(flightPlan);
-
-			pathNum++;
-			plan = "     Path " + pathNum + ": " + flightPlan.get(0).NAME;
-			int index = 1;
-			StringBuilder planBuilder = new StringBuilder(plan);
-			while(index < flightPlan.size())
+			if(flightPlan != null)
 			{
-				planBuilder.append(" -> ").append(flightPlan.get(index).NAME);
-				index++;
-			}
-			plan = planBuilder.toString();
-			String cost = String.format("%1$,.2f", COST);
+				COST = getPathCost(flightPlan);
+				TIME = getPathTime(flightPlan);
 
-			plan += ". Time: " + TIME + " Cost: " + cost + "\n";
-			output.add(plan);
+				pathNum++;
+				plan = "     Path " + pathNum + ": " + flightPlan.get(0).NAME;
+				int index = 1;
+				StringBuilder planBuilder = new StringBuilder(plan);
+				while(index < flightPlan.size())
+				{
+					planBuilder.append(" -> ").append(flightPlan.get(index).NAME);
+					index++;
+				}
+				plan = planBuilder.toString();
+				String cost = String.format("%1$,.2f", COST);
+
+				plan += ". Time: " + TIME + " Cost: " + cost + "\n";
+				output.add(plan);
+			}
 		}
 
 		if(pathNum == 0)
@@ -149,9 +155,10 @@ class FlightData
 
 	/**
 	 * Returns a list of all paths between two cities.
-	 * @param path A list of cities already in the path, should start as just the source city in the list.
+	 *
+	 * @param path        A list of cities already in the path, should start as just the source city in the list.
 	 * @param destination The destination city
-	 * @param paths A list of all paths, starts empty and each path is added when completed
+	 * @param paths       A list of all paths, starts empty and each path is added when completed
 	 */
 	private void getPaths(List<City> path, City destination, List<List<City>> paths)
 	{
@@ -203,6 +210,7 @@ class FlightData
 
 	/**
 	 * Returns the object representing a city in the graph
+	 *
 	 * @param name The name of the city
 	 * @return The City object in the graph with the same name as the input
 	 */
@@ -222,6 +230,7 @@ class FlightData
 
 	/**
 	 * Returns the total cost of a path
+	 *
 	 * @param path A list of City objects in the path
 	 * @return The total cost of the path
 	 */
@@ -231,23 +240,25 @@ class FlightData
 		double output = 0;
 
 		int index = 0;
-		while(index < path.size() - 1)
-		{
-			output += getCost(path.get(index).NAME, path.get(index + 1).NAME);
+		if(path != null)
+			while(index < path.size() - 1)
+			{
+				output += getCost(path.get(index).NAME, path.get(index + 1).NAME);
 
-			if(debug)
-				System.out.println(
-						"getPathCost: " + path.get(index).NAME + " -> " + path.get(index + 1).NAME + "\noutput += " +
-						getCost(path.get(index).NAME, path.get(index + 1).NAME) + " = " + output);
+				if(debug)
+					System.out.println("getPathCost: " + path.get(index).NAME + " -> " + path.get(index + 1).NAME +
+					                   "\noutput += " + getCost(path.get(index).NAME, path.get(index + 1).NAME) +
+					                   " = " + output);
 
-			index++;
-		}
+				index++;
+			}
 
 		return output;
 	}
 
 	/**
 	 * Returns the total time of a path
+	 *
 	 * @param path A list of City objects in the path
 	 * @return The total time to traverse the given path
 	 */
@@ -257,11 +268,65 @@ class FlightData
 		int output = 0;
 
 		int index = 0;
-		while(index < path.size() - 1)
+		if(path != null)
+			while(index < path.size() - 1)
+			{
+				output += getTime(path.get(index).NAME, path.get(index + 1).NAME);
+				index++;
+			}
+
+		return output;
+	}
+
+	/**
+	 * @param paths List paths
+	 * @param time  True sorts paths by time, False sorts paths by cost
+	 * @return
+	 */
+	public List<List<City>> sortPaths(List<List<City>> paths, boolean time)
+	{
+		List<City> firstPath = null, secondPath = null, thirdPath = null;
+		double firstWeight = -1, secondWeight = -1, thirdWeight = -1;
+		double weight;
+
+		for(List<City> current : paths)
 		{
-			output += getTime(path.get(index).NAME, path.get(index + 1).NAME);
-			index++;
+			if(time)
+				weight = getPathTime(current);
+			else
+				weight = getPathCost(current);
+
+			//If the first spot is empty or greater than the current path
+			if(weight < firstWeight || firstWeight == -1)
+			{
+				//Move all paths down to make room in the first spot
+				thirdPath = secondPath;
+				thirdWeight = secondWeight;
+				secondPath = firstPath;
+				secondWeight = firstWeight;
+				firstPath = current;
+				firstWeight = weight;
+			}
+			//Otherwise, if the second spot is empty or a worse path than the current one
+			else if(weight < secondWeight || secondWeight == -1)
+			{
+				thirdPath = secondPath;
+				thirdWeight = secondWeight;
+				secondPath = current;
+				secondWeight = weight;
+			}
+			//Otherwise, if the third spot is empty or a worse path than the current one
+			else if(weight < thirdWeight || thirdWeight == -1)
+			{
+				thirdPath = current;
+				thirdWeight = weight;
+			}
 		}
+
+		List<List<City>> output = new ArrayList<>();
+		output.add(firstPath);
+		output.add(secondPath);
+		output.add(thirdPath);
 
 		return output;
 	}
--- src/FlightPlanner.java
+++ src/FlightPlanner.java
@@ -4,25 +4,37 @@ class FlightPlanner
 {
 	public static void main(String[] args)
 	{
-		String[] paths = {"Dallas|Austin|60|120", "Dallas|Houston|70|150", "Dallas|Costa Rica|175|480",
-		                  "Austin|Dallas|65|130", "Austin|Costa Rica|180|410", "Costa Rica|Frankfurt|245|650",
-		                  "Austin|London|230|590", "Austin|Frankfurt|240|630", "Frankfurt|London|110|125",
-		                  "London|Houston|210|540", "Houston|Costa Rica|180|420", "Costa Rica|Austin|180|420"};
+		String[] paths = {"Dallas|Austin|60|120",
+		                  "Dallas|Houston|70|150",
+		                  "Dallas|Costa Rica|175|480",
+		                  "Austin|Dallas|65|130",
+		                  "Austin|Costa Rica|180|410",
+		                  "Costa Rica|Frankfurt|245|650",
+		                  "Austin|London|230|590",
+		                  "Austin|Frankfurt|240|630",
+		                  "Frankfurt|London|110|125",
+		                  "London|Houston|210|540",
+		                  "Houston|Costa Rica|180|420",
+		                  "Costa Rica|Austin|180|420"};
 
-		String[] flights = {"Middle|Fate|C", "Dallas|Frankfurt|C", "Frankfurt|Dallas|T", "Austin|London|T"};
+		String[] flights = {"Dallas|Frankfurt|C", "Frankfurt|Dallas|T", "Austin|London|T"};
 		FlightData flightData = new FlightData();
 
 		flightData.createGraph(paths);
 
 		int flightNum = 1;
 
-		for(String flight: flights)
+		for(String flight : flights)
 		{
 			String[] flightInfo = flight.split("\\|");
-			System.out.println("Flight " + flightNum + ": " + flightInfo[0] + " to " + flightInfo[1] + " (" +
-			                   flightInfo[2] + ")");
-			List<String> flightPaths = flightData.printFlightPaths(flightInfo[0], flightInfo[1]);
-			for(String flightPath: flightPaths)
+			System.out.println(
+					"Flight " + flightNum + ": " + flightInfo[0] + " to " + flightInfo[1] + " (" + flightInfo[2] +
+					")");
+			boolean time = true;
+			if(flightInfo[2].equals("C"))
+				time = false;
+			List<String> flightPaths = flightData.printFlightPaths(flightInfo[0], flightInfo[1], time);
+			for(String flightPath : flightPaths)
 				System.out.print(flightPath);
 			System.out.println();
 		}
